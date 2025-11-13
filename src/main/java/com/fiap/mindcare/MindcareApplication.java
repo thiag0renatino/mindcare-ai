ab90e2699fde4d6b58a2fc2f5e3b445e5c325566
Thiago <thiagorenatino@gmail.com>
2025-11-13 19:19:49 -0300

    primeiro commit: estrutura básico do projeto

---
+++ src/main/java/com/fiap/mindcare/MindcareApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.mindcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/mindcare/MindcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.mindcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
